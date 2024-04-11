c9216e0a51c15878860158c515f63a537fa89613
Aaron Gil Rubinos <142986554+AaronRubinos@users.noreply.github.com>
2024-04-11 19:24:48 +0800

    Add files via upload

---
+++ Java/Class and Object/Demo.java
@@ -0,0 +1,26 @@
+/**
+ * Demo
+ */
+class Calculator    {
+
+    int a;
+
+    public int add(int n1, int n2)    {
+        int r = n1 + n2;
+        return r;
+    }
+}
+
+public class Demo {
+
+    public static void main(String[] args) {
+        int num1=4; 
+        int num2=5;
+
+        Calculator calc = new Calculator();
+
+        int result = calc.add(num1, num2);
+
+        System.out.println(result);
+    }
+}
